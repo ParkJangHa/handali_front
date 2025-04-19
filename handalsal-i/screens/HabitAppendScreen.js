f36e2c9d3ac1447e7720af82d96b3dabb5b88d5d
Min0522-sku <wkdalstj0522@gmail.com>
2025-04-19 16:14:37 +0900

    design: 습관추가화면 ui 개선 디자인

--- handalsal-i/screens/HabitAppendScreen.js
+++ handalsal-i/screens/HabitAppendScreen.js
@@ -7,11 +7,11 @@ import {
   Image,
   Dimensions
 } from "react-native";
+
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const HabitAppendScreen = ({ route, navigation }) => {
-  // 이전 화면에서 전달된 데이터
-  const { category, habits = [] } = route.params; // 여러 개의 습관 배열 받기
+  const { category, habits = [] } = route.params;
   const today = new Date();
   const formattedDate = `${today.getFullYear()}-${(today.getMonth() + 1)
     .toString()
@@ -20,24 +20,23 @@ const HabitAppendScreen = ({ route, navigation }) => {
   const [progress, setProgress] = useState(85);
 
   const anotherCategory = () => {
-    navigation.navigate("Category"); // 카테고리 선택 화면으로 이동
+    navigation.navigate("Category");
   };
 
   const handalStart = () => {
     navigation.navigate("HandalStart", {
       category,
-      habits, // 선택한 습관 리스트 전달
+      habits,
     });
   };
 
-  // 카테고리별 데이터 정의
   const categoryData = {
     활동: {
       label: "활동",
       image: require("../assets/activityLogo.png"),
     },
-    지적: {
-      label: "지적",
+    지능: {
+      label: "지능",
       image: require("../assets/intelligenceLogo.png"),
     },
     예술: {
@@ -47,100 +46,81 @@ const HabitAppendScreen = ({ route, navigation }) => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.container}>
-        {/* 날짜 및 제목 */}
-        <Text style={styles.dateText}>{formattedDate}</Text>
-        <Text style={styles.title}>습관을 추가했어요!</Text>
-  
-        {/* 진행 바 */}
-        <View style={styles.progressBar}>
-          <Image
-            source={require("../assets/probar.png")}
-            style={styles.backgroundBar}
-          />
-          <View style={[styles.foregroundWrapper, { width: `${progress}%` }]}>
-            <Image
-              source={require("../assets/probarlevel.png")}
-              style={styles.foregroundBar}
-            />
-          </View>
+    <View style={styles.container}>
+      <Image source={require("../assets/Category/Weve.png")} style={styles.img} resizeMode="stretch" />
+      <Text style={styles.dateText}>{formattedDate}</Text>
+      <Text style={styles.title}>습관을 추가했어요!</Text>
+
+      <View style={styles.progressBar}>
+        <Image source={require("../assets/probar.png")} style={styles.backgroundBar} />
+        <View style={[styles.foregroundWrapper, { width: `${progress}%` }]}>
+          <Image source={require("../assets/probarlevel.png")} style={styles.foregroundBar} />
         </View>
       </View>
-  
-      <View style={styles.secondContainer}>
-        {/* 선택한 항목 표시 */}
-        <View style={styles.selectionContainer}>
-          {/* 선택된 습관 목록 */}
-          <View style={styles.habitContainer}>
-            <View style={styles.habitBox}>
-            {habits.map((habit, index) => (
-                <Text key={index} style={styles.habitText}>
-                {habit}
-                </Text>  
-            ))}
-          </View>
-          </View>
-  
-          <View style={styles.categoryContainer}>
-            <Image
-              source={categoryData[category].image}
-              style={styles.categoryImage}
-            />
-            <Text style={styles.categoryText}>{categoryData[category].label}</Text>
-  
-            {/* 추가 버튼 */}
-            <View style={styles.rowContainer}>
-              <TouchableOpacity
-                style={styles.anotherButton}
-                onPress={anotherCategory}
-              >
-                <Text style={styles.anotherButtonText}>
-                  다른 습관 추가
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.addButton} onPress={handalStart}>
-                <Text style={styles.addButtonText}>시작하기</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+
+      <Image source={categoryData[category].image} style={styles.categoryImage} />
+      <Text style={styles.categoryText}>{categoryData[category].label}</Text>
+
+      <View style={styles.selectionContainer}>
+        <View style={styles.habitContainer}>
+          {habits.map((habit, index) => (
+            <Text key={index} style={styles.habitText}>{habit}</Text>
+          ))}
+        </View>
+
+        <View style={styles.rowContainer}>
+          <TouchableOpacity style={styles.anotherButton} onPress={anotherCategory}>
+            <Text style={styles.anotherButtonText}>다른 습관 추가</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.addButton} onPress={handalStart}>
+            <Text style={styles.addButtonText}>시작하기</Text>
+          </TouchableOpacity>
         </View>
       </View>
+
+      <Image source={require("../assets/Category/B_weve.png")} style={styles.background} resizeMode="stretch" />
     </View>
-  ); 
+  );
 };
 
-// 스타일 정의
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     padding: SCREEN_WIDTH * 0.05,
-    backgroundColor: "#FFFDF0",
-    marginTop: -SCREEN_WIDTH * 0.06,
+    backgroundColor: "#FFBF80",
+    marginTop: -SCREEN_WIDTH * 0.07,
+  },
+  img: {
+    top: 0,
+    position: "absolute",
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT * 0.25,
+    zIndex: 0,
   },
   dateText: {
     fontSize: SCREEN_WIDTH * 0.06,
     fontWeight: "bold",
-    color: "#000",
+    color: "#2D5D6B",
     alignSelf: "flex-start",
   },
   title: {
     fontSize: SCREEN_WIDTH * 0.08,
     fontWeight: "bold",
+    color: "#2D5D6B",
     alignSelf: "flex-start",
   },
   progressBar: {
-    width: "90%",
+    width: "100%",
     height: SCREEN_HEIGHT * 0.01,
     justifyContent: "center",
-    marginVertical: SCREEN_HEIGHT * 0.02,
+    marginVertical: SCREEN_HEIGHT * 0.03,
   },
   backgroundBar: {
     width: "100%",
     height: "100%",
     position: "absolute",
-    borderRadius: SCREEN_WIDTH * 0.02,
+    borderRadius: 10,
   },
   foregroundWrapper: {
     height: "100%",
@@ -150,70 +130,49 @@ const styles = StyleSheet.create({
   foregroundBar: {
     width: "100%",
     height: "100%",
-    borderRadius: SCREEN_WIDTH * 0.02,
+    borderRadius: 10,
   },
-  secondContainer:{
-    flex: 6,
-    alignItems: "center",
-    backgroundColor: "#FFFDF0",
+  categoryImage: {
+    marginTop: SCREEN_HEIGHT * 0.07,
+    width: SCREEN_WIDTH * 0.3,
+    height: SCREEN_WIDTH * 0.3,
+    marginBottom: SCREEN_HEIGHT * 0.02,
+  },
+  categoryText: {
+    fontSize: SCREEN_WIDTH * 0.04,
+    fontWeight: "bold",
+    color: "#2D5D6B",
+    marginBottom: SCREEN_HEIGHT * 0.08,
   },
   selectionContainer: {
-    flex: 1,
     alignItems: "center",
     width: "100%",
   },
   habitContainer: {
     width: "100%",
-    height: SCREEN_HEIGHT * 0.5,
-    backgroundColor: "#F8B66C",
-    borderTopLeftRadius: SCREEN_WIDTH * 0.15,
-    borderTopRightRadius: SCREEN_WIDTH * 0.15,
+    height: SCREEN_HEIGHT * 0.25,
     justifyContent: "center",
     alignItems: "center",
     padding: SCREEN_WIDTH * 0.05,
-    marginBottom: -SCREEN_WIDTH * 0.6,
-  },
-  habitBox: {
-    flex: 1,
+    marginBottom: SCREEN_HEIGHT * 0.03,
   },
   habitText: {
-    fontSize: SCREEN_WIDTH * 0.05,
+    fontSize: SCREEN_WIDTH * 0.08,
     fontWeight: "bold",
-    color: "#000",
+    color: "#2D5D6B",
     textAlign: "center",
     marginBottom: SCREEN_HEIGHT * 0.01,
-  },
-  categoryContainer: {
-    width: "100%",
-    height: SCREEN_HEIGHT * 0.6,
-    backgroundColor: "#F8E36E",
-    borderTopLeftRadius: SCREEN_WIDTH * 0.5,
-    borderTopRightRadius: SCREEN_WIDTH * 0.5,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  categoryImage: {
-    width: SCREEN_WIDTH * 0.6,
-    height: SCREEN_WIDTH * 0.6,
-    marginBottom: SCREEN_HEIGHT * 0.02,
-  },
-  categoryText: {
-    fontSize: SCREEN_WIDTH * 0.06,
-    fontWeight: "bold",
-    color: "#000",
-    marginBottom: SCREEN_HEIGHT * 0.03,
   },
   rowContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    gap: SCREEN_WIDTH * 0.02,
-    marginBottom: SCREEN_HEIGHT * 0.04,
+    gap: SCREEN_WIDTH * 0.05,
   },
   anotherButton: {
-    flex: 1.4,
-    backgroundColor: "rgba(255, 253, 240, 0.5)",
-    height: SCREEN_HEIGHT * 0.07, // 버튼의 고정 높이
+    backgroundColor: "#ECF7F7",
+    width: SCREEN_WIDTH * 0.3,
+    height: SCREEN_HEIGHT * 0.07,
     borderRadius: SCREEN_WIDTH * 0.05,
     justifyContent: "center",
     alignItems: "center",
@@ -222,15 +181,12 @@ const styles = StyleSheet.create({
   anotherButtonText: {
     fontSize: SCREEN_WIDTH * 0.035,
     fontWeight: "bold",
-    color: "rgba(0, 0, 0, 0.6)",
-    textAlign: "center",
-    lineHeight: SCREEN_HEIGHT * 0.07, // 버튼 높이와 동일하게 설정
-    textAlignVertical: "center", // 텍스트 수직 정렬
+    color: "rgba(45, 93, 107, 0.6)",
   },
   addButton: {
-    flex: 2,
-    backgroundColor: "#F8B66C",
-    height: SCREEN_HEIGHT * 0.07, // 버튼의 고정 높이
+    width: SCREEN_WIDTH * 0.5,
+    backgroundColor: "#76D6F4",
+    height: SCREEN_HEIGHT * 0.07,
     borderRadius: SCREEN_WIDTH * 0.05,
     justifyContent: "center",
     alignItems: "center",
@@ -239,14 +195,15 @@ const styles = StyleSheet.create({
   addButtonText: {
     fontSize: SCREEN_WIDTH * 0.045,
     fontWeight: "bold",
-    color: "#000",
-    textAlign: "center",
-    lineHeight: SCREEN_HEIGHT * 0.07, // 버튼 높이와 동일하게 설정
-    textAlignVertical: "center", // 텍스트 수직 정렬
+    color: "#2D5D6B",
   },
-  
+  background: {
+    position: "absolute",
+    bottom: 0,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT * 0.45,
+    zIndex: -1,
+  },
 });
 
-
 export default HabitAppendScreen;
-
